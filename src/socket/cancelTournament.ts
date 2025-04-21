be891f77b66f2a899dedf0adf62d948cfb28663e
francismars <11526661+francismars@users.noreply.github.com>
2025-04-21 19:06:51 +0000

    cancel tournament

--- src/socket/cancelTournament.ts
+++ src/socket/cancelTournament.ts
@@ -6,7 +6,11 @@ import {
   getLNURLPsFromID,
 } from '../state/lnurlpState';
 import createLNURLW from '../calls/LNBits/createLNURLW';
-import { setIDToLNURLW, setLNURLWToID } from '../state/lnurlwState';
+import {
+  getLNURLWFromID,
+  setIDToLNURLW,
+  setLNURLWToID,
+} from '../state/lnurlwState';
 import { deleteSocketFromSession } from '../state/sessionState';
 
 export async function cancelTournament(socket: Socket) {
@@ -16,51 +20,56 @@ export async function cancelTournament(socket: Socket) {
     depositcount: number;
     lnurlw?: string;
   } = { depositcount: 0 };
+  const LNURLW = getLNURLWFromID(sessionID);
+  if (LNURLW) {
+    console.log(`${dateNow()} [${sessionID}] Found an existing LNURLw.`);
+    return;
+  }
   const tournamentInfo = getGameInfoFromID(sessionID);
   if (!tournamentInfo) {
     console.log(`${dateNow()} [${sessionID}] No tournament information found.`);
     return;
   }
-  if (tournamentInfo.players) {
-    const depositcount = tournamentInfo.players.size;
+  if (!tournamentInfo.players) {
     console.log(
-      `${dateNow()} [${sessionID}] Found an ongoing tournament with ${depositcount} deposits.`
+      `${dateNow()} [${sessionID}] No players found in tournament information.`
     );
-    const players = tournamentInfo.players;
-    response.depositcount = depositcount;
-    if (depositcount > 0) {
-      const lastLNURLPtournament = getLNURLPsFromID(sessionID);
-      if (
-        lastLNURLPtournament &&
-        lastLNURLPtournament[0].mode == 'TOURNAMENT'
-      ) {
-        const amount = lastLNURLPtournament[0].min;
-        console.log(
-          `${dateNow()} [${sessionID}] Creating LNRURLw with ${amount} sats and ${depositcount} uses.`
-        );
-        let lnurlw = await createLNURLW(
-          Math.floor(amount * 0.95),
-          depositcount
-        );
-        if (lnurlw) {
-          console.log(
-            `${dateNow()} [${sessionID}] Created LNURLw ${lnurlw.id}.`
-          );
-          setIDToLNURLW(sessionID, {
-            id: lnurlw.id,
-            lnurlw: lnurlw.lnurl,
-            maxWithdrawals: depositcount,
-            claimedCount: 0,
-          });
-          setLNURLWToID(lnurlw.id, sessionID);
-          response.lnurlw = lnurlw.lnurl;
-        }
-      }
-    } else if (depositcount == 0) {
-      deleteLNURLPsFromSession(sessionID);
-      deleteGameInfoByID(sessionID);
-      deleteSocketFromSession(sessionID);
-    }
+    return;
+  }
+  const depositcount = tournamentInfo.players.size;
+  console.log(
+    `${dateNow()} [${sessionID}] Found an ongoing tournament with ${depositcount} deposits.`
+  );
+  response.depositcount = depositcount;
+  if (depositcount == 0) {
+    deleteLNURLPsFromSession(sessionID);
+    deleteGameInfoByID(sessionID);
+    deleteSocketFromSession(sessionID);
+    socket.emit('rescanceltourn', response);
+    return;
+  }
+  const LNURLPs = getLNURLPsFromID(sessionID);
+  if (!LNURLPs || LNURLPs[0].mode != 'TOURNAMENT') {
+    console.log(
+      `${dateNow()} [${sessionID}] No LNURLp found for tournament information.`
+    );
+    return;
+  }
+  const amount = LNURLPs[0].min;
+  console.log(
+    `${dateNow()} [${sessionID}] Creating LNRURLw with ${amount} sats and ${depositcount} uses.`
+  );
+  let lnurlw = await createLNURLW(Math.floor(amount * 0.95), depositcount);
+  if (lnurlw) {
+    console.log(`${dateNow()} [${sessionID}] Created LNURLw ${lnurlw.id}.`);
+    setIDToLNURLW(sessionID, {
+      id: lnurlw.id,
+      lnurlw: lnurlw.lnurl,
+      maxWithdrawals: depositcount,
+      claimedCount: 0,
+    });
+    setLNURLWToID(lnurlw.id, sessionID);
+    response.lnurlw = lnurlw.lnurl;
   }
   console.log(
     `${dateNow()} [${sessionID}] Sending cancel tournament information.`
--- src/state/cleanupState.ts
+++ src/state/cleanupState.ts
@@ -5,9 +5,14 @@ import { deleteGameInfoByID, serializeGameInfoFromID } from './gameState';
 import { deleteLNURLWFromSession, getIDFromLNURLW } from './lnurlwState';
 import { deleteSocketFromSession } from './sessionState';
 import { promises as fs } from 'fs';
+import { deleteLNURLPsFromSession, getLNURLPsFromID } from './lnurlpState';
 
 export function handleEndOfSession(sessionID: string) {
   appendGameInfotoJSON(sessionID);
+  const LNURLPs = getLNURLPsFromID(sessionID);
+  if (LNURLPs) {
+    deleteLNURLPsFromSession(sessionID);
+  }
   const LNURLWFromID = getIDFromLNURLW(sessionID);
   if (LNURLWFromID) {
     deleteLNURLW(LNURLWFromID);
