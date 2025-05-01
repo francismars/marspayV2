b02a07f5ab611e8cc9d0875fe023999a71aad19c
francismars <11526661+francismars@users.noreply.github.com>
2025-05-01 18:57:17 +0000

    cleanupInactiveSessions and delete unused event

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -6,7 +6,6 @@ import { gameFinished, gameInfos } from './game';
 import { createWithdrawalPostGame, postGameInfo } from './postGame';
 import { cancelP2P } from './cancelP2P';
 import { getPracticeMenuInfos } from './practiceMenu';
-import { normalizeIP } from '../utils/ip';
 import { getTournamentMenuInfos } from './tournament';
 import { cancelTournament } from './cancelTournament';
 import { getNostrP2PMenuInfos } from './nostrP2PMenu';
@@ -17,11 +16,6 @@ export default function registerSocketHandlers(io: Server) {
   });
 
   io.on('connection', (socket: Socket) => {
-    const realIP = normalizeIP(socket.handshake.address); // TODO: change when NGINX is set up
-    console.log(
-      `${dateNow()} [${socket.data.sessionID}] connected with IP ${realIP}.`
-    );
-
     // TODO: Change to getP2PMenuInfos
     socket.on('getGameMenuInfos', async () => {
       await getP2PMenuInfos(socket);
@@ -67,8 +61,6 @@ export default function registerSocketHandlers(io: Server) {
       await createWithdrawalPostGame(socket);
     });
 
-    socket.on('disconnect', () => {
-      console.log(`${dateNow()} [${socket.data.sessionID}] Disconnected.`);
-    });
+    socket.on('disconnect', () => {});
   });
 }
--- src/state/cleanupState.ts
+++ src/state/cleanupState.ts
@@ -1,10 +1,11 @@
 import deleteLNURLW from '../calls/LNBits/deleteLNURLW';
 import { deleteGameInfoByID } from './gameState';
 import { deleteLNURLWFromSession, getIDFromLNURLW } from './lnurlwState';
-import { deleteSocketFromSession } from './sessionState';
+import { deleteSocketFromSession, getAllIDtoSocket } from './sessionState';
 import { deleteLNURLPsFromSession, getLNURLPsFromID } from './lnurlpState';
 import { appendGameInfotoJSON } from '../utils/json';
 import { deleteKind1sFromSession } from './nostrState';
+import { CLEANUP_INTERVAL, INACTIVITY_THRESHOLD } from '../consts/values';
 
 export function handleEndOfSession(
   sessionID: string,
@@ -24,3 +25,19 @@ export function handleEndOfSession(
   deleteGameInfoByID(sessionID);
   deleteSocketFromSession(sessionID);
 }
+
+function cleanupInactiveSessions(inactivityThreshold: number) {
+  const now = Date.now();
+  const allSessions = getAllIDtoSocket();
+
+  for (const [sessionID, session] of allSessions) {
+    if (now - session.lastSeen > inactivityThreshold) {
+      console.log(`Cleaning up inactive session: ${sessionID}`);
+      handleEndOfSession(sessionID, false);
+    }
+  }
+}
+
+setInterval(() => {
+  cleanupInactiveSessions(INACTIVITY_THRESHOLD);
+}, CLEANUP_INTERVAL);
