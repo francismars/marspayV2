06e3557a8e1636b13fa2596bebd63fea2b9e21ff
francismars <11526661+francismars@users.noreply.github.com>
2025-05-02 20:44:20 +0000

    activated slpits

--- src/routes/paidLNURL.ts
+++ src/routes/paidLNURL.ts
@@ -110,8 +110,7 @@ router.post('/', ipFilter, (req: Request, res: Response) => {
       console.error("Couldn't find SocketID to send notification of payment");
       return;
     }
-    const splitsActive = false;
-    if (splitsActive) paySplits(sessionID, amount);
+    paySplits(sessionID, amount);
     io.to(socketID!).emit('updatePayments', serializeGameInfoFromID(sessionID));
     res.status(200).send('OK');
   }
